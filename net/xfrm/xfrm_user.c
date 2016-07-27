7b98f3dab5c6dd30e259f207e502932e099bf1f3
Vegard Nossum <vegard.nossum@oracle.com>
2016-07-27 08:03:18 +0200

    xfrm: get rid of incorrect WARN
    AFAICT this message is just printed whenever input validation fails.
    This is a normal failure and we shouldn't be dumping the stack over it.
    
    Looks like it was originally a printk that was maybe incorrectly
    upgraded to a WARN:
    
    commit 62db5cfd70b1ef53aa21f144a806fe3b78c84fab
    Author: stephen hemminger <shemminger@vyatta.com>
    Date:   Wed May 12 06:37:06 2010 +0000
    
        xfrm: add severity to printk
    
    Change-Id: Id464bdf25477568c1f1ef5450f3d9db142360323
    Cc: Stephen Hemminger <stephen@networkplumber.org>
    Cc: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

--- net/xfrm/xfrm_user.c
+++ net/xfrm/xfrm_user.c
@@ -2147,7 +2147,7 @@ static int xfrm_add_acquire(struct sk_buff *skb, struct nlmsghdr *nlh,
 
 	err = verify_newpolicy_info(&ua->policy);
 	if (err)
-		goto bad_policy;
+		goto free_state;
 
 	/*   build an XP */
 	xp = xfrm_policy_construct(net, &ua->policy, attrs, &err);
@@ -2179,8 +2179,6 @@ static int xfrm_add_acquire(struct sk_buff *skb, struct nlmsghdr *nlh,
 
 	return 0;
 
-bad_policy:
-	WARN(1, "BAD policy passed\n");
 free_state:
 	kfree(x);
 nomem:
