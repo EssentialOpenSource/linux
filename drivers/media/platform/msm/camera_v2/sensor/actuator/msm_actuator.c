cc06de2bf6a1b20899a81e07f48a522ec64f59fe
Jean-Baptiste Theou <jb@essential.com>
2017-09-06 10:07:16 -0700

    msm_actuator: increase usleep for mono sensor
    The mono sensor requires up to 14ms to be properly powered up
    
    Change-Id: I7eaaefcd51eccb581b367156a5a16eb3f2ff18c0

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -1782,7 +1782,7 @@ static int32_t msm_actuator_power_up(struct msm_actuator_ctrl_t *a_ctrl)
 	}
 
 	/* VREG needs some delay to power up */
-	usleep_range(2000, 3000);
+	usleep_range(12000, 14000);
 	a_ctrl->actuator_state = ACT_ENABLE_STATE;
 
 	CDBG("Exit\n");
