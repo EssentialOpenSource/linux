b95ed46dedc8636170e7e89f7374281fa282f29d
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2018-02-11 05:44:21 -0500

    media: dmxdev: fix error code for invalid ioctls
    [ Upstream commit a145f64c6107d3aa5a7cec9f8977d04ac2a896c9 ]
    
    Returning -EINVAL when an ioctl is not implemented is a very
    bad idea, as it is hard to distinguish from other error
    contitions that an ioctl could lead. Replace it by its
    right error code: -ENOTTY.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/media/dvb-core/dmxdev.c
+++ drivers/media/dvb-core/dmxdev.c
@@ -4407,7 +4407,7 @@ static int dvb_demux_do_ioctl(struct file *file,
 	default:
 		pr_err("%s: unknown ioctl code (0x%x)\n",
 			__func__, cmd);
-		ret = -ENOIOCTLCMD;
+		ret = -ENOTTY;
 		break;
 	}
 	mutex_unlock(&dmxdev->mutex);
