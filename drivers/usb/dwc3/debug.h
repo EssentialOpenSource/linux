69f73afa3e432e0af47b748b4fcca60c54e8b89f
Jean-Baptiste Théou <jb@essential.com>
2018-04-20 11:35:54 +0900

    dwc3: make debugfs entries optional
    Change-Id: Ia83723065c04754b99e76f71a2b5ac75cb8c1a3e
    Signed-off-by: Jean-Baptiste Théou <jb@essential.com>

--- drivers/usb/dwc3/debug.h
+++ drivers/usb/dwc3/debug.h
@@ -216,7 +216,7 @@ static inline const char *dwc3_gadget_event_type_string(u8 event)
 
 void dwc3_trace(void (*trace)(struct va_format *), const char *fmt, ...);
 
-#ifdef CONFIG_DEBUG_FS
+#ifdef CONFIG_USB_DWC3_DEBUG_FS
 extern void dbg_event(u8, const char*, int);
 extern void dbg_print(u8, const char*, int, const char*);
 extern void dbg_done(u8, const u32, int);
