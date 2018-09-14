a91ac482a192e5e3737f48b10ac434b7c26d859a
Jean-Baptiste Théou <jb@essential.com>
2018-09-14 15:18:28 +0900

    policy_engine: allow max current to be above src_caps
    Per PD standard, Capability Mismatch flag can be used to notify the
    source than the max current may be above capability, but we should
    still proceed with the request.
    
    Reported-by: Badhri Jagan Sridharan <badhri@google.com>
    Suggested-by: Benson Leung <bleung@google.com>
    
    Change-Id: Iede87f604ceacd2c4b00f3fa47a4009c68798b80
    Signed-off-by: Jean-Baptiste Théou <jb@essential.com>

--- drivers/usb/pd/policy_engine.c
+++ drivers/usb/pd/policy_engine.c
@@ -817,11 +817,14 @@ static void usbpd_set_state(struct usbpd *pd, enum usbpd_state next_state)
 		break;
 
 	case PE_SRC_NEGOTIATE_CAPABILITY:
+		/* Per standard, the operating current needs to be
+		 * supported, but the max may be above capability
+		 * as long as the cap mismatch bit is set */
 		if (PD_RDO_OBJ_POS(pd->rdo) != 1 ||
 			PD_RDO_FIXED_CURR(pd->rdo) >
 				PD_SRC_PDO_FIXED_MAX_CURR(*default_src_caps) ||
-			PD_RDO_FIXED_CURR_MINMAX(pd->rdo) >
-				PD_SRC_PDO_FIXED_MAX_CURR(*default_src_caps)) {
+			(PD_RDO_FIXED_CURR_MINMAX(pd->rdo) >
+				PD_SRC_PDO_FIXED_MAX_CURR(*default_src_caps) && !PD_RDO_MISMATCH(pd->rdo))) {
 			/* send Reject */
 			ret = pd_send_msg(pd, MSG_REJECT, NULL, 0, SOP_MSG);
 			if (ret) {
