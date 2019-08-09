af6408f8bfe7b84f2833f4cc4045c6559dde6a5d
Jean-Baptiste Theou <jb@essential.com>
2019-08-09 13:08:12 +0200

    q6asm: fix patch integration
    Change-Id: I7610f8e1a4abb2a05e64de52a727e5de04fe0df5
    Signed-off-by: Jean-Baptiste Theou <jb@essential.com>

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -1953,9 +1953,6 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 					__func__, buf_index);
 				spin_unlock_irqrestore(&port->dsp_lock,
 								dsp_flags);
-				spin_unlock_irqrestore(
-					&(session[session_id].session_lock),
-					flags);
 				return -EINVAL;
 			}
 			if (lower_32_bits(port->buf[buf_index].phys) !=
@@ -2047,9 +2044,6 @@ static int32_t q6asm_callback(struct apr_client_data *data, void *priv)
 					__func__, buf_index);
 				spin_unlock_irqrestore(&port->dsp_lock,
 								dsp_flags);
-				spin_unlock_irqrestore(
-					&(session[session_id].session_lock),
-					flags);
 				return -EINVAL;
 			}
 			port->buf[buf_index].used = 0;
